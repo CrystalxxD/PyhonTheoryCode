150b20047c731b9c9c8fbd7a5eb2069fcae88b00
CrystalxxD <chris.wong948@education.nsw.gov.au>
2025-03-13 14:40:30 +1100

    Integration

---
+++ Integration/caculator.py
@@ -0,0 +1,8 @@
+import math_operation 
+
+
+print('What numbers do u want to add together')
+a = int(input('What is the first number: '))
+b = int(input('What is the second number: '))
+
+print(f'The answer is ', math_operation.add_numbers(a, b))
